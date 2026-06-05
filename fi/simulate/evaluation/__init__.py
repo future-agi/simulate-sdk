851f1c8df5e0c87df44f4c9c8a7a30d07fba972d
Nikhil Pareek <nick.coolvyas@gmail.com>
2026-06-05 11:27:44 +0530

    Retire legacy simulation source

--- fi/simulate/evaluation/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ai_eval import evaluate_agent_report, evaluate_report
-
-__all__ = ["evaluate_agent_report", "evaluate_report"]
-
--- fi/simulate/recording/__init__.py
+++
@@ -1,5 +0,0 @@
-from .room_recorder import RoomRecorder
-
-__all__ = ["RoomRecorder"]
-
-
